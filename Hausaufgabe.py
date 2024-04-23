4243bce1e78255e302de7ca7cbe4c813e2ef02c4
ErikRichter120198 <erik.richter@edu.techstarter.de>
2024-04-23 15:28:20 +0200

    Ich hoffe man kann diese Änderung lesen

--- Hausaufgabe.py
+++ Hausaufgabe.py
@@ -1,1 +1,3 @@
-print("Apfel Banane Lol ist mein 6. Versuch da was zu machen")
+print("Apfel Banane Lol ist mein 6. Versuch da was zu machen und hier schreibe ich irgendwas")
+
+print("Langsam ist es auch gut wenn es nicht funktioniert")
